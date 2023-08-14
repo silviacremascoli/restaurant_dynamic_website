a887df85e5316097fc7973387e31cfaa0d83f26b
silviacremascoli <silcremascoli@gmail.com>
2023-08-14 16:58:10 +0200

    Reorganised folder

--- files/index.php
+++ files/index.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>"Franklin's Awesome Diner"</title>
-    <link href="/styles/styles.css" rel="stylesheet">
+    <link href="/scss/app.css" rel="stylesheet">
 </head>
 <body>
 <div class="container">
