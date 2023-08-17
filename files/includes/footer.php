c3177e0d1e8b337b7b23e932d6365a3afce11d4c
silviacremascoli <silcremascoli@gmail.com>
2023-08-17 15:24:17 +0200

    Added working hours

--- files/includes/footer.php
+++ files/includes/footer.php
@@ -7,8 +7,11 @@
             <strong>Address</strong>: <span>32631 Rowe Mews, 30424 East Maeganfort</span>
         </div>
         <div class="column">
-            <strong>Hours</strong>: <br>
-            <span>Tuesday - Thursday</span>
+            <strong>Hours</strong> <br>
+            <span><em>Tuesday - Thursday:</em> <span>12:00 - 23:00</span></span>
+            <span><em>Friday - Saturday:</em> <span>12:00 - 24:00</span></span>
+            <span><em>Sunday:</em> <span>10:00 - 24:00</span></span>
+
         </div>
     </div>
     <?php include('store-hours.php') ?>
