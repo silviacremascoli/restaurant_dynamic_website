ae4f2fc03a3eaaa5a962ba690352eb3aec8fa1d1
silviacremascoli <silcremascoli@gmail.com>
2023-08-27 12:22:40 +0200

    Improved CSS

--- public_html/index.php
+++ public_html/index.php
@@ -3,7 +3,6 @@ define("TITLE", "Home | Franklin's Fine Dining");
 include('../includes/header.php'); ?>
 
     <div class="homepage">
-
         <h1>Welcome to Franklin's Fine Dining!</h1>
         <div class="description">
             <p>Here at Franklin's, we know that good food isn't just about how expensive the dish is. We're not pompous, we're proud. We're proud of our work, our quality, our environment, and our love for food and family.</p>
