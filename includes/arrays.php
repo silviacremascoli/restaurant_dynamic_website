196b0a6b885f983ac9e69aeee3b6f4496a3bb4b7
silviacremascoli <silcremascoli@gmail.com>
2023-08-28 16:57:07 +0200

    Added menu array and menu template

--- includes/arrays.php
+++ includes/arrays.php
@@ -44,4 +44,32 @@ $teamMembers = array(
     )
 );
 
+$menuItems = array(
+
+    "club-sandwich" => array(
+        "title" => "Club Sandwich",
+        "price" => 11,
+        "description" => "Bacon ipsum dolor sit amet fatback landjaeger ullamco pariatur. Nostrud laboris et, duis drumstick eiusmod kevin ut aliquip. Filet mignon short ribs tenderloin short loin kielbasa non pork chop aliqua. Jerky shank tri-tip capicola, non andouille tenderloin cupidatat turducken meatball pork beef eu shoulder jowl.",
+        "drink" => "Club Soda"
+    ),
+    "dill-salmon" => array(
+        "title" => "Lemon & Dill Salmon",
+        "price" => 18,
+        "description" => "Fish cod belly tempor ground round qui exercitation, jowl leberkas sed voluptate excepteur jerky. Reprehenderit veniam cow, quis in ribeye andouille eu pastrami eiusmod exercitation dolor.",
+        "drink" => "Chardonnay"
+    ),
+    "super-salad" => array(
+        "title" => "The Super Salad<sup>©</sup>",
+        "price" => 34,
+        "description" => "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.",
+        "drink" => "Jug o' Water"
+    ),
+    "mexican-barbacoa" => array(
+        "title" => "Mexican Barbacoa",
+        "price" => 23,
+        "description" => "I love fruitcake danish caramels. Tart danish pastry liquorice chocolate cake fruitcake. Bear claw gingerbread muffin I love apple pie apple pie tiramisu brownie chocolate. Sweet roll cotton candy cupcake gingerbread gummies jelly-o. Muffin I love croissant I love jelly-o brownie jelly beans. Toffee I love pastry.",
+        "drink" => "Beer with a Lime"
+    )
+)
+
 ?>
--- public_html/menu.php
+++ public_html/menu.php
@@ -1,1 +1,32 @@
-<?php include('dish.php') ?>
+<?php
+global$menuItems;
+define("TITLE", "Menu | Franklin's Fine Dining");
+include("../includes/header.php");
+
+?>
+
+<div class="menu">
+
+    <h1>Our delicious menu</h1>
+    <p>
+        Like our team, our menu is very small - but dang, does it ever pack a punch!
+    </p>
+    <p>
+        <em>Click any menu item to learn more about it ;-)</em>
+    </p>
+
+    <ul>
+        <?php
+        foreach ($menuItems as $dish => $item) {
+        ?>
+            <li><a href="dish.php?item=<?php echo $dish; ?>"><?php echo $item["title"]; ?></a><span class="price"><sup>$</sup><?php echo $item["price"];  ?></span></li>
+
+
+        <?php } ?>
+    </ul>
+
+</div>
+
+<?php
+include("../includes/footer.php");
+?>
